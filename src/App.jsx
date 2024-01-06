78b3fc134a7943b837ba76cfa875ff5820feed24
Ahmed Kotb <akotb621@gmail.com>
2024-01-06 10:31:49 +0300

    feat:programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 const BASE_URL = "http://localhost:8000";
 function App() {
   const [cities, setCities] = useState([]);
@@ -37,10 +38,7 @@ function App() {
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -50,7 +48,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import style from "./Button.module.css";
+function Button({ children, onClick, type }) {
+  return (
+    <button className={`${style.btn} ${style[type]}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+export default Button;
